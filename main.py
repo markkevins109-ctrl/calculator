7e8a0beb4905a94646f1681563e645c1e8a7251d
markkevins109-ctrl <mail2mark123@gmail.com>
2025-08-20 12:50:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
-class Numbers(BaseModel):
-    a: int
-    b: int
-
-@app.post("/add")
-def add_numbers(numbers: Numbers):
-    return {"result": numbers.a + numbers.b}
+@app.get("/add")
+def add_numbers(a: int, b: int):
+    return {"result": a + b}
